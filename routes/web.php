b0f4d70169cf9050efa0ae9b0de5492851660910
erikka-22 <iwasaki.pingoo@gmail.com>
2022-07-27 01:12:29 +0900

    コントローラの簡単な実装まで完了

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MembersController;
+use App\Http\Controllers\GroupsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::resources([
+    'members' => MembersController::class,
+    'groups' => GroupsController::class,
+]);
